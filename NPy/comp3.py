4bad5ff0b0c5837f4e5ec810fa47c5e294c4d1fa
samiyaalrabhi11 <106156638+samiyaalrabhi11@users.noreply.github.com>
2022-05-30 14:41:05 +0400

    Add files via upload

---
+++ NPy/comp3.py
@@ -0,0 +1,10 @@
+a= in(input('Enter num a: '))
+b= in(input('Enter num b: '))
+c= in(input('Enter num c: '))
+if a>b and a>c:
+    print('a is biggest')
+else b>a and b>c:
+    print('b is biggest')    
+else:
+    print('c is biggest')
+   
